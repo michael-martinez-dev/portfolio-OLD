8f0f22a5f80d0069b8244fe2880001dc221ab7fd
Michael Marinez <80479622+MixedMachine@users.noreply.github.com>
2022-02-03 21:51:13 -0700

    initialize repo

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import About from './components/About';
+
+function App() {
+  return (
+    <main  className=" App text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+    </main>
+  );
+}
+
+export default App;
